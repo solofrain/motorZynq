0438c4f874988de1575c6c50e0827dace10cbbbe
Ji Li <solofrain@gmail.com>
2024-11-07 22:28:39 -0500

    Update IOC

--- iocs/zynqIoc/iocBoot/ioczynq/st.cmd
+++ iocs/zynqIoc/iocBoot/ioczynq/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/zynq
+#!../../bin/linux-arm/zynq
 
 #- You may have to change zynq to something else
 #- everywhere it appears in this file
@@ -9,18 +9,20 @@ epicsEnvSet("Sys", "Det")
 epicsEnvSet("Dev", "Mtr")
 epicsEnvSet("Port", "ZMTR")
 
+cd $(TOP)
+
 ## Register all support components
-dbLoadDatabase("../../dbd/zynq.dbd",0,0)
+dbLoadDatabase("dbd/zynq.dbd",0,0)
 zynq_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-#dbLoadRecords("./zynqMotor.substitutions")
-#dbLoadRecords("../../db/zynqMotor.substitutions", "MOTOR=${MOTOR}, P=$(Sys), PORT=$(Port)")
+dbLoadTemplate("db/zynqMotor.substitutions", "Sys=$(Sys),Dev=$(Dev),Port=$(Port)")
 
-dbLoadRecords("$(MOTOR)/db/asyn_motor.db","P=$(Sys),M={${Dev}-Ax:X},DTYP=asynMotor,PORT=ZMTR,ADDR=0,DESC=X,EGU=mm,DIR=Pos,VELO=1,VBAS=.1,ACCL=.2,BDST=0,BVEL=1,BACC=.2,MRES=.0025,PREC=4,DHLM=100,DLLM=-100,INIT=")
-dbLoadRecords("$(MOTOR)/db/asyn_motor.db","P=$(Sys),M={${Dev}-Ax:Y},DTYP=asynMotor,PORT=ZMTR,ADDR=1,DESC=Y,EGU=mm,DIR=Pos,VELO=1,VBAS=.1,ACCL=.2,BDST=0,BVEL=1,BACC=.2,MRES=.0025,PREC=4,DHLM=100,DLLM=-100,INIT=")
+#dbLoadRecords("$(MOTOR)/db/asyn_motor.db","P=$(Sys),M={${Dev}-Ax:X},DTYP=asynMotor,PORT=ZMTR,ADDR=0,DESC=X,EGU=mm,DIR=Pos,VELO=1,VBAS=.1,ACCL=.2,BDST=0,BVEL=1,BACC=.2,MRES=.0025,PREC=4,DHLM=0,DLLM=0,INIT=")
+#dbLoadRecords("$(MOTOR)/db/asyn_motor.db","P=$(Sys),M={${Dev}-Ax:Y},DTYP=asynMotor,PORT=ZMTR,ADDR=1,DESC=Y,EGU=mm,DIR=Pos,VELO=1,VBAS=.1,ACCL=.2,BDST=0,BVEL=1,BACC=.2,MRES=.01,PREC=4,DHLM=0,DLLM=0,INIT=")
 
-zynqMotorCreateController($(Port), "ZMTR_MM", 2, 250, 1000)
+#zynqMotorCreateController($(Port), "ZMTR_MM", 2, 250, 1000)
+zynqMotorCreateController($(Port), "ZMTR_MM", 2, 10000, 10000)
 
 iocInit()
 
